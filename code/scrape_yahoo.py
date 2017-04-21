d394578e06b32875e43c4aa941ffe7c1d1a1b6d4
nateGeorge <wordsforthewise@gmail.com>
2017-04-20 21:32:57 -0600

    FEAT: save as gzip and load stocks

--- code/scrape_yahoo.py
+++ code/scrape_yahoo.py
@@ -16,27 +16,40 @@ QUOTE_BASE_URL = "https://download.finance.yahoo.com/d/quotes.csv?"
 
 STOCKLIST = "../stockdata/goldstocks.txt"
 
-# load stocklist
+def download_stocks(stocklist=STOCKLIST):
+    # load stocklist
+    with open(stocklist) as f:
+        stocks = f.read().strip('\n').split('\n')
 
-with open(STOCKLIST) as f:
-    stocks = f.read().strip('\n').split('\n')
+    # download each stock
+    for s in stocks:
+        scrape_url = HIST_BASE_URL + 's=' + s + '&a=0&b=0&c=0&d=' + MONTH + '&e=' + DAY + '&f=' + YEAR
+        res = req.get(scrape_url)
+        if not res.ok:
+            print('problem! :' + res.status_code)
+            continue
+
+        test = res.content.strip(b'\n').split(b'\n')
+        labels = test[0].decode('ascii').split(',')
+        datadict = OrderedDict()
+        for line in test[1:]:
+            data = line.decode('ascii').split(',')
+            datadict.setdefault(labels[0], []).append(datetime.datetime.strptime(data[0], '%Y-%m-%d'))
+            for d, l in zip(data[1:], labels[1:]):
+                datadict.setdefault(l, []).append(float(d))
+
+        df = pd.DataFrame(datadict)
+        df.to_csv('../stockdata/' + s + '.csv.gz', index=False, compression='gzip')
 
 
-for s in stocks:
-    scrape_url = HIST_BASE_URL + 's=' + s + '&a=0&b=0&c=0&d=' + MONTH + '&e=' + DAY + '&f=' + YEAR
-    res = req.get(scrape_url)
-    if not res.ok:
-        print('problem! :' + res.status_code)
-        continue
+def load_stocks(stocks=['GLD']):
+    dfs = {}
+    for s in stocks:
+        df = pd.read_csv('../stockdata/' + s + '.csv.gz', index_col=0, parse_dates=True)
+        dfs[s] = df
 
-    test = res.content.strip(b'\n').split(b'\n')
-    labels = test[0].decode('ascii').split(',')
-    datadict = OrderedDict()
-    for line in test[1:]:
-        data = line.decode('ascii').split(',')
-        datadict.setdefault(labels[0], []).append(datetime.datetime.strptime(data[0], '%Y-%m-%d'))
-        for d, l in zip(data[1:], labels[1:]):
-            datadict.setdefault(l, []).append(float(d))
+    return dfs
 
-    df = pd.DataFrame(datadict)
-    df.to_csv('../stockdata/' + s + '.csv', index=False)
+
+if __name__ == "__main__":
+    download_stocks()
